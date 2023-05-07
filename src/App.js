8e84d1b38a15f65bb0e8733454477fc8c031f404
THT <thehelpfultipper@gmail.com>
2023-05-06 20:02:24 -0700

    Refactor fetch action from child component to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
+import { useEffect, useState } from "react";
 import BackgroundImage from './Components/BackgroundImage';
 
 import './App.css';
 
+const IMAGES = [],
+      QUOTES = [];
+
 function App() {
-  return <BackgroundImage />;
+  const [images, setImages] = useState(IMAGES);
+  const [quotes, setQuotes] = useState(QUOTES);
+
+  const fetchImgDetails = async (id) => {
+    let res = await fetch(`https://api.unsplash.com/photos/${id}`, {
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`,
+      },
+    });
+
+    let image = await res.json();
+
+    return {
+      id: image.id,
+      alt: image.alt_description,
+      url: image.urls.regular,
+      link: image.links.html,
+      local: {
+        name: image.location.name,
+        city: image.location.city,
+        country: image.location.country,
+      },
+    };
+  };
+
+  const fetchImages = async () => {
+    let res = await fetch(
+      "https://api.unsplash.com/search/photos?orientation=landscape&query=europe+asia",
+      {
+        headers: {
+          Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`,
+        },
+      }
+    );
+
+    let images = await res.json();
+
+    let allImages = [];
+
+    // Get details for each image
+    for (let img of images.results) {
+      let details = await fetchImgDetails(img.id); // single img details {}
+      allImages.push(details);
+    }
+
+    setImages(allImages);
+  };
+
+  const fetchQuotes = async () => {
+    let res = await fetch("https://type.fit/api/quotes");
+    let quotes = await res.json();
+
+    setQuotes(quotes);
+  };
+
+  useEffect(() => {
+    fetchImages();
+    fetchQuotes();
+  }, []);
+  
+  return <BackgroundImage images={images} quotes={quotes} />;
 }
 
 export default App;
--- src/Components/BackgroundImage.js
+++ src/Components/BackgroundImage.js
@@ -1,13 +1,46 @@
+import { useState } from "react";
 import Card from "./Card/Card";
 
 import styles from "./BackgroundImage.module.css";
 
+const getRdmItem = (list) => {
+  return list[Math.floor(Math.random() * list.length)];
+};
+
 const BackgroundImage = (props) => {
+  let [randomImg, setRandomImg] = useState();
+  let [randomQuote, setRandomQuote] = useState();
+
+  if (props.images.length > 0 && props.quotes.length > 0) {
+    randomImg = getRdmItem(props.images);
+    randomQuote = getRdmItem(props.quotes);
+  } else {
+    return <h2>Loading...</h2>;
+  }
+
+  let {
+      local: { city, country, name },
+    } = randomImg,
+    place;
+
+  if ((!city && !country && !name) || !city || !country) {
+    place = "Photo on Unsplash";
+  } else if (name) {
+    place = name;
+  } else if (country && city) {
+    place = `${city}, ${country}`;
+  }
+
   return (
-    <div>
-      <Card />
+    <div
+      className={styles.bgimg_main_wrapper}
+      style={{ backgroundImage: `url(${randomImg.url})` }}
+    >
+      <Card quote={randomQuote} />
       <div className={styles.bgimg_location_link}>
-        <a>Location</a>
+        <a href={randomImg.link} target="_blank" rel="noopener">
+          {place}
+        </a>
       </div>
     </div>
   );
