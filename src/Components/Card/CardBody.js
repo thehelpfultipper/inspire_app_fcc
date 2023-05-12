a673b5176836e9730f72580b4de935595819b07e
THT <thehelpfultipper@gmail.com>
2023-05-11 19:40:55 -0700

    Dynamic text in static card height

--- src/Components/Card/CardBody.js
+++ src/Components/Card/CardBody.js
@@ -1,11 +1,22 @@
 import styles from "./CardBody.module.css";
 
 const CardBody = (props) => {
-  let { text, author } = props.quote;
-console.log(text.length)
+  let { text, author } = props.quote,
+    textSize;
+
+  if (text.length > 175) {
+    textSize = "1.3em";
+  } else if (text.length > 110) {
+    textSize = "1.4em";
+  } else {
+    textSize = "1.5em";
+  }
+
   return (
     <div className={styles.cardbody_wrapper}>
-      <p className={`${styles.cardbody_text} ${styles.md_text}`}>{text}</p>
+      <p className={styles.cardbody_text} style={{ fontSize: textSize }}>
+        {text}
+      </p>
       <div className={styles.cardbody_separator}></div>
       <p className={styles.cardbody_author}>{author}</p>
     </div>
