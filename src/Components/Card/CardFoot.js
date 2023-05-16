1056855f57be60a84c0630f5bb1c5703c99a1a3a
THT <thehelpfultipper@gmail.com>
2023-05-15 20:36:46 -0700

    Style changes

--- src/Components/Card/CardFoot.js
+++ src/Components/Card/CardFoot.js
@@ -8,19 +8,19 @@ const CardFoot = () => {
   return (
     <div className={styles.cardfoot_wrapper}>
       <div className={styles.cardfoot_socials}>
-        <a href="">
+        <a href="#">
           <img src="/icons8-facebook-50.png"></img>
         </a>
-        <a href="">
+        <a href="#">
           <img src="/icons8-instagram-50.png"></img>
         </a>
-        <a href="">
+        <a href="#">
           <img src="/icons8-twitter-squared-50.png"></img>
         </a>
       </div>
       <div className={styles.cardfoot_nxtbtn}>
         <button type="button" onClick={ctx.nxtBtn}>
-          <img src="/icons8-more-than-50.png"></img>
+          <img src="/icons8-more-than-50.png" alt=""></img>
         </button>
       </div>
     </div>
