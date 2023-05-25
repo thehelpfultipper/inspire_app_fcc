1f19294dbf6506506e04c9dfafe6aeaa72d13b14
THT <thehelpfultipper@gmail.com>
2023-05-25 08:53:45 -0700

    Adjust loader style for body

--- src/Components/LoadAnimation.js
+++ src/Components/LoadAnimation.js
@@ -2,19 +2,21 @@ import styles from "./LoadAnimation.module.css";
 
 const LoadAnimation = () => {
   return (
-    <div className={styles.preloader}>
-      <div className={styles.preloader_text}>Loading</div>
-      <div className={`${styles.preloader_wrapper} ${styles.firstWrap}`}>
-        <div className={styles.preloader_dot}></div>
-      </div>
-      <div className={`${styles.preloader_wrapper} ${styles.secondWrap}`}>
-        <div className={styles.preloader_dot}></div>
-      </div>
-      <div className={`${styles.preloader_wrapper} ${styles.thirdWrap}`}>
-        <div className={styles.preloader_dot}></div>
-      </div>
-      <div className={`${styles.preloader_wrapper} ${styles.fourthWrap}`}>
-        <div className={styles.preloader_dot}></div>
+    <div className={styles.preloader_body}>
+      <div className={styles.preloader}>
+        <div className={styles.preloader_text}>Loading</div>
+        <div className={`${styles.preloader_wrapper} ${styles.firstWrap}`}>
+          <div className={styles.preloader_dot}></div>
+        </div>
+        <div className={`${styles.preloader_wrapper} ${styles.secondWrap}`}>
+          <div className={styles.preloader_dot}></div>
+        </div>
+        <div className={`${styles.preloader_wrapper} ${styles.thirdWrap}`}>
+          <div className={styles.preloader_dot}></div>
+        </div>
+        <div className={`${styles.preloader_wrapper} ${styles.fourthWrap}`}>
+          <div className={styles.preloader_dot}></div>
+        </div>
       </div>
     </div>
   );
