390226c07545eeb6e5423c1a24488a686e64be8f
THT <thehelpfultipper@gmail.com>
2023-07-16 11:05:14 -0700

    Add public path to imgs

--- src/Components/Card/CardFoot.js
+++ src/Components/Card/CardFoot.js
@@ -1,29 +1,32 @@
 import { useContext } from "react";
 import NextContext from "../../context/next-context";
+import ImgContext from "../../context/img-context";
 
 import styles from "./CardFoot.module.css";
 
 const CardFoot = () => {
   let ctx = useContext(NextContext);
+  let {path} = useContext(ImgContext);
+
   return (
     <div className={styles.cardfoot_wrapper}>
       <div className={styles.cardfoot_socials}>
         <a href="https://thehelpfultipper.com/" target="_blank" rel="noreferrer">
-          <img src="/tht-logo-nobg-90.png" alt=""></img>
+          <img src={path + "/tht-logo-nobg-90.png"} alt=""></img>
         </a>
         <a href="https://www.instagram.com/dm.tht/?hl=en" target="_blank" rel="noreferrer">
-          <img src="/icons8-instagram-90.png" alt=""></img>
+          <img src={path + "/icons8-instagram-90.png"} alt=""></img>
         </a>
         <a href="https://pinterest.com/thehelpfultipper/_created/" target="_blank" rel="noreferrer">
-          <img src="/icons8-pinterest-96.png" alt=""></img>
+          <img src={path + "/icons8-pinterest-96.png"} alt=""></img>
         </a>
         <a href="www.linkedin.com/in/kmerkuri97" target="_blank" rel="noreferrer">
-          <img src="/icons8-linkedin-96.png" alt=""></img>
+          <img src={path + "/icons8-linkedin-96.png"} alt=""></img>
         </a>
       </div>
       <div className={styles.cardfoot_nxtbtn}>
         <button type="button" onClick={ctx.nxtBtn}>
-          <img src="/icons8-more-than-50.png" alt=""></img>
+          <img src={path + "/icons8-more-than-50.png"} alt=""></img>
         </button>
       </div>
     </div>
--- src/Components/Card/CardHead.js
+++ src/Components/Card/CardHead.js
@@ -1,9 +1,14 @@
+import { useContext } from "react";
+import ImgContext from "../../context/img-context";
+
 import styles from "./CardHead.module.css";
 
 const CardHead = () => {
+    let {path} = useContext(ImgContext);
+
     return (
         <div className={styles.cardhead_wrapper}>
-            <img src="/icons8-quote-left-96.png"></img>
+            <img src= {path + "/icons8-quote-left-96.png"} alt=""></img>
         </div>
     )
 }
